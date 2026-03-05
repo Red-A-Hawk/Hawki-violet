32842c854a2e9425a421ab8aff200086308cd10f
Johan Hovold <johan+linaro@kernel.org>
2026-03-05 18:27:38 +0100

    UPSTREAM: iommu/qcom: fix device leak on of_xlate()
    Make sure to drop the reference taken to the iommu platform device
    when looking up its driver data during of_xlate().
    
    Note that commit e2eae09939a8 ("iommu/qcom: add missing put_device()
    call in qcom_iommu_of_xlate()") fixed the leak in a couple of error
    paths, but the reference is still leaking on success and late failures.
    
    Fixes: 0ae349a0f33f ("iommu/qcom: Add qcom_iommu")
    Change-Id: Ifa9b1f2dc16e126119eecac189c067520c7f286c
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Quince <quinceroms@gmail.com>
    (cherry picked from commit 9395474a4ebdebf9018d8fbef14ce324a1559dba)
    (cherry picked from commit e30bb8767531d9f9108b6396d1a600fce58ab162)
    (cherry picked from commit b8f5e92f59f0fe80dfca2a08eb1fdeb379fd6b17)
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/iommu/qcom_iommu.c
+++ drivers/iommu/qcom_iommu.c
@@ -534,6 +534,7 @@ static int qcom_iommu_of_xlate(struct device *dev, struct of_phandle_args *args)
 		return -EINVAL;
 
 	qcom_iommu = platform_get_drvdata(iommu_pdev);
+	put_device(&iommu_pdev->dev);
 
 	/* make sure the asid specified in dt is valid, so we don't have
 	 * to sanity check this elsewhere, since 'asid - 1' is used to
